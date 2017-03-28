85d8e0175e946542ebac35d77036147b41f71dfc
josep roman <pepinho@gmail.com>
2017-03-28 11:38:01 +0200

    prove login register ionic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,45 +41,102 @@ angular.module('myApp.controllers', [])
 
 }])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function ($scope, $rootScope, $ionicModal, $timeout, $localStorage,AuthFactory) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = $localStorage.getObject('userinfo','{}');
+    $scope.registration = {};
+    $scope.loggedIn = false;
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    if(AuthFactory.isAuthenticated()) {
+        $scope.loggedIn = true;
+        $scope.username = AuthFactory.getUsername();
+    }
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Open the login modal
+    $scope.login = function () {
+        $scope.modal.show();
+    };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
+        $localStorage.storeObject('userinfo',$scope.loginData);
+
+        AuthFactory.login($scope.loginData);
+
+        $scope.closeLogin();
+    };
+
+    $scope.logOut = function() {
+       AuthFactory.logout();
+        $scope.loggedIn = false;
+        $scope.username = '';
+    };
+
+    $rootScope.$on('login:Successful', function () {
+        $scope.loggedIn = AuthFactory.isAuthenticated();
+        $scope.username = AuthFactory.getUsername();
+    });
+
+
+
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/register.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.registerform = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeRegister = function () {
+        $scope.registerform.hide();
+    };
+
+    // Open the login modal
+    $scope.register = function () {
+        $scope.registerform.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doRegister = function () {
+        console.log('Doing registration', $scope.registration);
+        $scope.loginData.username = $scope.registration.username;
+        $scope.loginData.password = $scope.registration.password;
+
+        AuthFactory.register($scope.registration);
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function () {
+            $scope.closeRegister();
+        }, 1000);
+    };
+
+    $rootScope.$on('registration:Successful', function () {
+        $scope.loggedIn = AuthFactory.isAuthenticated();
+        $scope.username = AuthFactory.getUsername();
+        $localStorage.storeObject('userinfo',$scope.loginData);
+    });
 })
 
 .controller('PlaylistsCtrl', function($scope) {
